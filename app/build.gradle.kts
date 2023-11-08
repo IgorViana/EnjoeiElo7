cccab6dbc3c99b61c6896c15f81f6c95e8e43515
Igor <igorviana40@hotmail.com>
2023-11-08 07:49:36 -0300

    Updating SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.enjoeielo7"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.enjoeielo7"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
